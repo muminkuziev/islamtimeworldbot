5ffb115c3290f8491c0f4949d14d9e4d93ef4544
muminkuziev <islamtimebot@gmail.com>
2026-06-07 09:33:16 +0200

    Create __init__.py

---
+++ handlers/handlers/__init__.py
@@ -0,0 +1,5 @@
+from .start import register_start
+from .prayer import register_prayer
+from .quran import register_quran
+from .hadith import register_hadith
+from .settings import register_settings
